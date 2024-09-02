e9045b681cbae26a76160946d4e4ed88f26b01a7
Kessy Abarenkov <kessy@Kessys-MBP.lan>
2024-09-02 12:37:55 +0300

    Update BLAST reference databases.

--- run_setup.sh
+++ run_setup.sh
@@ -15,10 +15,10 @@ echo "Removing old blast database files ..."
 rm -fr massblaster_plutof_rel/
 
 # Download BLAST database files
-echo "Downloading blast database files from https://app.plutof.ut.ee/filerepository/view/6908093 ..."
-wget https://s3.hpc.ut.ee/plutof-public/original/6da1f051-9606-447c-a9d0-8cef05136c94.gz
-tar -xzvf 6da1f051-9606-447c-a9d0-8cef05136c94.gz
-rm 6da1f051-9606-447c-a9d0-8cef05136c94.gz
+echo "Downloading blast database files from https://app.plutof.ut.ee/filerepository/view/7363196 ..."
+wget https://s3.hpc.ut.ee/plutof-public/original/fa1dc323-f32a-4c21-b453-47601e3e8204.gz
+tar -xzvf fa1dc323-f32a-4c21-b453-47601e3e8204.gz
+rm fa1dc323-f32a-4c21-b453-47601e3e8204.gz
 
 # end here
 echo "Done"
